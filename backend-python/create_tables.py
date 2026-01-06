3c07305b0e84b237970b22156cacdd7821405dcc
paulovmoura <pviniciuslazaro@gmail.com>
2026-01-06 17:48:38 -0300

    limpeza back

--- backend-python/create_tables.py
+++
@@ -1,7 +0,0 @@
-from app import create_app, db
-from app.models.study_material import StudyMaterial
-
-app = create_app()
-with app.app_context():
-    db.create_all()
-    print("Tables created successfully, including StudyMaterial.")
